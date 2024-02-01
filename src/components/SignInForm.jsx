12b65113e803a3804a9b166a1254e3913250eee7
irishabhnegi <rishabhnegi175@gmail.com>
2024-02-01 23:50:17 +0530

    model window open and close

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,6 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
 
 const schema = z.object({
   email: z.string().email({ message: 'Invalid email address' }),
@@ -8,6 +9,12 @@ const schema = z.object({
 });
 
 const SignInForm = () => {
+  const [model, setModel] = useState(false);
+
+  console.log(model);
+
+  const handleModel = () => {};
+
   const {
     register,
     handleSubmit,
@@ -31,7 +38,7 @@ const SignInForm = () => {
           {...register('email')}
           type='text'
           placeholder='Email address'
-          className='w-[22rem] border-gray-400 border text-lg px-4 py-[.9rem] rounded-md'
+          className='w-[22rem] border-gray-400 border text-lg text-gray-800 px-4 py-[.9rem] rounded-md focus:border-0 focus:outline-none focus:ring-2 focus:ring-gray-500 '
         />
         {errors.email && (
           <p className='text-red-500 text-sm'>{errors.email.message}</p>
@@ -40,23 +47,30 @@ const SignInForm = () => {
           {...register('password')}
           type='password'
           placeholder='Password'
-          className='w-[22rem] border-gray-400 border text-lg px-4 py-[.9rem] rounded-md '
+          className='w-[22rem] border-gray-400 border text-lg text-gray-800 px-4 py-[.9rem] rounded-md focus:border-0 focus:outline-none focus:ring-2 focus:ring-gray-500 '
         />
         {errors.password && (
           <p className='text-red-500 text-sm'>{errors.password.message}</p>
         )}
         <button
           type='submit'
-          className='w-[22rem] px-4 py-[.9rem] bg-green-400 rounded-md  font-bold text-white text-lg '>
+          className='w-[22rem] px-4 py-[.9rem] bg-green-400 rounded-md  font-bold text-white text-lg'>
           Log in
         </button>
       </form>
 
       <button
         type='button'
-        className='w-[22rem] px-4 py-[.9rem] bg-gray-500 rounded-md mt-4 font-bold text-white text-lg '>
+        className='w-[22rem] px-4 py-[.9rem] bg-gray-500 rounded-md mt-4 font-bold text-white text-lg '
+        onClick={() => setModel(!model)}>
         Create new account
       </button>
+
+      {model && (
+        <div
+          onClick={() => setModel(!model)}
+          className='absolute top-0 left-0 w-full h-screen bg-gray-300 opacity-70 z-50'></div>
+      )}
     </section>
   );
 };
